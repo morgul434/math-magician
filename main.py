43391b49362b4593f7bc89727acdf1ee73880746
obelea23 <obelea23@htl-kaindorf.at>
2026-03-17 11:31:37 +0100

    added subtraction function

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
   # Math Magician Main File
   # usage: math operator operand1 operand2
-  def main():
+from unittest import result
+
+
+def sub(a, b): 
+	return a - b
+
+
+def main():
       print("Welcome to Math Magician!")
 
 
-  def main():
+def main():
       import sys
 
       op = sys.argv[1]
       a = float(sys.argv[2])
-      b = float(sys.argv[3])    
+      b = float(sys.argv[3])
 
+      if op == "sub" or op == "-":
+            result = sub(a, b)
 
-  if __name__ == "__main__":
+      
+      print(f"Result: {result}")
+
+if __name__ == "__main__":
       main()
